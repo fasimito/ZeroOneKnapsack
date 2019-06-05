3c5482d09e6dd1c6d00ac69280189a83ccd3c4c9
jate <fasimito@163.com>
2019-06-05 23:00:12 +0800

    commit 0/1 Snackpack implements

--- src/com/fasimito/zoknapsack/ZOKnapsack.java
+++ src/com/fasimito/zoknapsack/ZOKnapsack.java
@@ -1,29 +1,40 @@
 package com.fasimito.zoknapsack;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ZOKnapsack {
 
     private List<Node> list;
+    private int[] roads;  // record the roads of the value
 
     public void init(List<Node> list){
         this.list = list;
+        roads=new int[list.size()];
     }
 
     public int knapsack(int index, int cap){  // start from zero
-        if(index<0)
+        if(index==0)
             return 0;
-        Node node = list.get(index);
+        Node node = list.get(index-1);
         if(node.getWeight()>cap){
+            roads[index-1]=0;
             return knapsack(index-1,cap);
         }else{
             int value1 = knapsack(index-1,cap);
             int value2 = knapsack(index-1,cap-node.getWeight())+node.getValue();
-            if(value1>value2)
+            if(value1>value2) {
+                roads[index-1]=0;
                 return value1;
-            else
+            }else {
+                roads[index-1]=1;
                 return value2;
+            }
+        }
+    }
+
+    public void printRoads(){
+        for(int i=0; i<roads.length;i++){
+            System.out.println(roads[i]);
         }
     }
 
@@ -33,12 +44,13 @@ public class ZOKnapsack {
         Node node2 = new Node(6,6);
         Node node3 = new Node(4,4);
         List<Node> ls = new ArrayList<>();
+        ls.add(node3);
         ls.add(node1);
         ls.add(node2);
-        ls.add(node3);
         zoKnapsack.init(ls);
-        int result = zoKnapsack.knapsack(2,10);
+        int result = zoKnapsack.knapsack(3,20);
         System.out.println(result);
+        zoKnapsack.printRoads();
     }
 
 }
